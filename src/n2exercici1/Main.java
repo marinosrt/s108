ce17d3a364f8d483d0dfc63c298ffa498bc989c3
marinosrt <marina.rterol@gmail.com>
2023-01-15 20:19:11 +0100

    Second commit Level 2

---
+++ src/n2exercici1/Main.java
@@ -0,0 +1,30 @@
+package n2exercici1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<String> myList = Arrays.asList("Pau", "Edna", "Albert", "Marina", "Ariel", "Jan", "Ala");
+
+        System.out.println("Names that start with letter A and has exactly 3 characters: ");
+        System.out.println(findLetterA(myList));
+
+    }
+
+    public static List<String> findLetterA(List<String> myList){
+        List<String> LetterAList = new ArrayList<>();
+
+        myList.forEach(word -> {
+            if(word.startsWith("A") && word.length() == 3){
+                LetterAList.add(word);
+            }
+        });
+
+
+        return LetterAList;
+    }
+
+}
---
+++ src/n2exercici2/Main.java
@@ -0,0 +1,31 @@
+package n2exercici2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> myList = Arrays.asList(10, 23, 45, 567, 342, 3, 2);
+
+        System.out.println("The output list is:");
+        System.out.println(returnLettersAndNumbers(myList));
+
+    }
+
+    public static List<String> returnLettersAndNumbers(List<Integer> myList){
+        List<String> LetterNumberList = new ArrayList<>();
+
+        myList.forEach(number -> {
+            if(number % 2 == 0){
+                LetterNumberList.add("e" + number);
+            } else {
+                LetterNumberList.add("o" + number);
+            }
+        });
+
+        return LetterNumberList;
+    }
+
+}
